dc543735df2421a24e2aabdd47460b27d6f11f3c
NeiltonSeguins <seguins.neilton@gmailcom>
2023-11-08 14:57:43 -0300

    Vídeo 2.2 - Lidando com estados

--- src/context/CarrinhoContext.jsx
+++ src/context/CarrinhoContext.jsx
@@ -1,11 +1,13 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const CarrinhoContext = createContext();
 
-export const CarrinhoProvider = ({children}) => {
+export const CarrinhoProvider = ({ children }) => {
+  const [carrinho, setCarrinho] = useState([]);
+
   return (
-    <CarrinhoContext.Provider>
+    <CarrinhoContext.Provider value={{ carrinho, setCarrinho }}>
       {children}
     </CarrinhoContext.Provider>
-  )
-}
+  );
+};
