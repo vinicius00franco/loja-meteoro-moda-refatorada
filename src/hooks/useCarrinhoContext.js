fddcf721fe4f90c3d6d609cdc7f9a2715c90de52
NeiltonSeguins <seguins.neilton@gmailcom>
2023-11-10 14:51:11 -0300

    Vídeo-3.1 - Criando um hook customizado

---
+++ src/hooks/useCarrinhoContext.js
@@ -0,0 +1,34 @@
+import { useContext } from "react";
+import { CarrinhoContext } from "@/context/CarrinhoContext";
+
+export const useCarrinhoContext = () => {
+  const { carrinho, setCarrinho } = useContext(CarrinhoContext);
+
+  function adicionarProduto(novoProduto) {
+    const temOProduto = carrinho.some((itemDoCarrinho) => {
+      itemDoCarrinho.id === novoProduto.id;
+    });
+
+    if (!temOProduto) {
+      novoProduto.quantidade = 1;
+      return setCarrinho((carrinhoAnterior) => [
+        ...carrinhoAnterior,
+        novoProduto,
+      ]);
+    }
+
+    setCarrinho((carrinhoAnterior) =>
+      carrinhoAnterior.map((itemDoCarrinho) => {
+        if (itemDoCarrinho.id === novoProduto.id)
+          itemDoCarrinho.quantidade += 1;
+        return itemDoCarrinho;
+      })
+    );
+  }
+
+  return {
+    carrinho,
+    setCarrinho,
+    adicionarProduto,
+  };
+};
