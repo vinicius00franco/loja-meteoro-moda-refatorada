4d5d673c6f1e88c0f11cb202e1d4a53d3921c0c6
NeiltonSeguins <seguins.neilton@gmailcom>
2023-11-16 15:44:13 -0300

    Vídeo 5.5 - Conclusão do curso

--- src/context/CarrinhoContext.jsx
+++ src/context/CarrinhoContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer, useState } from "react";
+import { createContext, useEffect, useMemo, useReducer, useState } from "react";
 import { carrinhoReducer } from "../reducers/carrinhoReducer";
 
 export const CarrinhoContext = createContext();
@@ -10,6 +10,24 @@ export const CarrinhoProvider = ({ children }) => {
   const [carrinho, dispatch] = useReducer(carrinhoReducer, estadoInicial);
   const [quantidade, setQuantidade] = useState(0);
   const [valorTotal, setValorTotal] = useState(0);
+
+  const { totalTemp, quantidadeTemp } = useMemo(() => {
+    return carrinho.reduce(
+      (acumulador, produto) => ({
+        quantidadeTemp: acumulador.quantidadeTemp + produto.quantidade,
+        totalTemp: acumulador.totalTemp + produto.preco * produto.quantidade,
+      }),
+      {
+        quantidadeTemp: 0,
+        totalTemp: 0,
+      }
+    );
+  }, [carrinho]);
+
+  useEffect(() => {
+    setQuantidade(quantidadeTemp);
+    setValorTotal(totalTemp);
+  });
 
   return (
     <CarrinhoContext.Provider
